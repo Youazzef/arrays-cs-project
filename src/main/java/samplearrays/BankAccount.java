483f53489d529272c45fb447bcbc4dd51cbc8360
youssef ouazzani <youssefouazzani2006@gmail.com>
2026-09-25 23:58:24 +0100

    Bank Acc

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,26 +4,44 @@ public class BankAccount {
 
     String name;
     double currentBalance;
+    int numOfTrans = 0;
     //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
 
     public BankAccount(String name, int startingBalance){
-
+        this.name = name;
+        this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
-
+        if (amount < 0){
+            System.out.println("Failed deposit.");
+            return;
+        }
+        currentBalance += amount;
+        transactions[numOfTrans] = amount;
+        numOfTrans += 1;
+        System.out.println("Account name : " + name + " Deposited amount : " + amount + " Current Balance : " + currentBalance);
     }
 
     public void withdraw(double amount){
-
+        if (amount < 0){
+            System.out.println("Failed withdrawal.");
+            return;
+        }
+        currentBalance -= amount;
+        transactions[numOfTrans] = -amount;
+        numOfTrans += 1;
     }
 
     public void displayTransactions(){
-
+        for (int i = 0 ; i < numOfTrans ; i++){
+            System.out.println(transactions[i]);
+        }
     }
 
     public void displayBalance(){
-
+        System.out.println(currentBalance);
     }
 
     public static void main(String[] args) {
